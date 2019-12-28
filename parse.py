4ea2d7f432b4260cab84b76d5bdce84f8055e720
Io <me@me.me>
2019-12-28 18:17:43 -0500

    Add precedences

--- parse.py
+++ parse.py
@@ -2,34 +2,74 @@
 import sys
 import lex
 
+def pp(x):
+    if isinstance(x, list):
+        return "({})".format("".join(map(pp, x)))
+    else:
+        return x[1]
+
+def flush_til(buf, out_q, lvl):
+    R = buf.pop()
+    while out_q:
+        if out_q[-1][1] > lvl:
+            break
+        H = out_q.pop()[0]
+        L = buf.pop()
+        R = [L, H, R]
+    return R
+
 def pparse(toks):
     tok = next(toks)
     if tok[2] == "rparen":
-        x, end = tok[1], True
+        x, end = tok, True
     elif tok[2] == "lparen":
         x, end = parse(toks, ")"), None
     else:
-        x, end = tok[1], None
+        x, end = tok, None
     return x, end
 
 def parse(toks, expected_end):
+    buf, out_q = [], []
+
     L, end = pparse(toks)
-    if end and L == expected_end:
+    if end and L[1] == expected_end:
         return "()"
+
+    buf.append(L)
 
     while True:
         H, end = pparse(toks)
-        if end and H == expected_end:
+        if end and H[1] == expected_end:
             break
 
         R, end = pparse(toks)
         assert not end
-        L = [L, H, R]
 
-    return L
+        right_assoc = 0
+        op = H[1]
+        if op == ';':
+            lvl = 4
+        elif op == "|":
+            lvl = 3
+            right_assoc = 1
+        elif op == ",":
+            lvl = 2
+        elif op == ".":
+            lvl = 1
+        elif op == ":":
+            lvl = 0
+            right_assoc = 1
+        else:
+            lvl = 1
+
+        buf.append(flush_til(buf, out_q, lvl - right_assoc))
+        buf.append(R)
+        out_q.append((H, lvl))
+
+    return flush_til(buf, out_q, 999)
 
 if __name__ == "__main__":
     s = sys.argv[1]
     toks = lex.lex(s)
     tree = parse(toks, "EOF")
-    print(tree)
+    print(pp(tree))
